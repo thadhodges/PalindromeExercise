59a77e3798d2b8c9dbbd1c79b3e337e7ca4a18f2
Thad <thadisfree@gmail.com>
2021-12-16 13:34:23 -0600

    final commit, exercise complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+            char[] chars = word.ToCharArray();
+            Array.Reverse(chars);
+            var reversed = new string(chars);
+            
+            if (reversed == word )
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,16 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("baker", false)]
+        [InlineData("QWERTREWQ", true)]
 
+        public void IsAPalindrome(string word, bool expected)
+        {
+            WordSmith challenger = new WordSmith();
+            bool actual = challenger.IsAPalindrome(word);
+            Assert.Equal(actual, expected);
         }
     }
 }
